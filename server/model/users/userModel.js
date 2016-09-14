1a292a66b6c5da30912dd9577b582086f1b72745
Donghee Lee <jacobdhlee@gmail.com>
2016-09-14 00:43:47 -0400

    add user schema model and bcrypt

--- server/model/users/userModel.js
+++ server/model/users/userModel.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const Promise = require('bluebird');
 
 var Schema = mongoose.Schema;
+const saltRound = 10;
 
 var UserSchema = new Schema({
 
@@ -30,3 +31,48 @@ var UserSchema = new Schema({
     }
   }
 })
+
+UserSchema.pre('save', (next) => {
+  let user = this;
+  if( !user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSaly(saltRound, (err, salt)=>{
+    if(err) {
+      next(err)
+    }
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if(err) {
+        next(err)
+      }
+      user.password = hash;
+      next();
+    })
+  })
+})
+
+UserSchema.methods.comparedPassword (password) => {
+  var savedPassword = this.password;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, savedPassword, (err, isMatch) => {
+      if(err) {
+        reject(err);
+      }
+        resolve(isMatch);
+    })
+  })
+}
+
+module.export = mongoose.model('User', UserSchema)
+
+
+
+
+
+
+
+
+
+
+
